779f10100920b685baa5ff6691eea777f871234b
dm1681 <dm1681@gmail.com>
2023-04-25 19:14:33 -0700

    updated settings

--- lua/hbar6/set.lua
+++ lua/hbar6/set.lua
@@ -3,13 +3,13 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = "~/.vim/undodir"
+vim.opt.undodir = ".vim/undodir"
 vim.opt.undofile = true
 
 
