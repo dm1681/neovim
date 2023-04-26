835b7acdfe2ec8ef5254ff0ec7d72c41323c2e7d
dm1681 <dm1681@gmail.com>
2023-04-25 19:59:13 -0700

    added more plugins

--- lua/hbar6/packer.lua
+++ lua/hbar6/packer.lua
@@ -5,22 +5,40 @@ return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
-
-
-
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-	-- this is a test comment
+
 	use 'folke/tokyonight.nvim'
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use('nvim-treesitter/playground')
 	use('theprimeagen/harpoon')
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
+	use('neoclide/coc.nvim')
+	use('vim-airline/vim-airline')
+	use('tpope/vim-commentary')
+	use('mg979/vim-visual-multi')
+	use('preservim/nerdtree')
+	use('ryanoasis/vim-devicons')
+	use('tiagofumo/vim-nerdtree-syntax-highlight')
+	use('Xuyuanp/nerdtree-git-plugin')
+
+
+
+
+	use({
+	    "kylechui/nvim-surround",
+	    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+	    config = function()
+		require("nvim-surround").setup({
+		    -- Configuration here, or leave empty to use defaults
+		})
+	    end
+	})
 
 	use {
 		'VonHeikemen/lsp-zero.nvim',
