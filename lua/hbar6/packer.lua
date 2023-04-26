727b0091199cc26a9cdd6358f0209f0d6d2ac975
dm1681 <dm1681@gmail.com>
2023-04-25 20:14:30 -0700

    update packer to be using only working packages and no coc

--- lua/hbar6/packer.lua
+++ lua/hbar6/packer.lua
@@ -12,20 +12,18 @@ return require('packer').startup(function(use)
 	}
 
 
-	use 'folke/tokyonight.nvim'
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use('nvim-treesitter/playground')
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	use('neoclide/coc.nvim')
-	use('vim-airline/vim-airline')
-	use('tpope/vim-commentary')
-	use('mg979/vim-visual-multi')
-	use('preservim/nerdtree')
-	use('ryanoasis/vim-devicons')
-	use('tiagofumo/vim-nerdtree-syntax-highlight')
-	use('Xuyuanp/nerdtree-git-plugin')
+ 	use 'folke/tokyonight.nvim'
+ 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+ 	use('nvim-treesitter/playground')
+ 	use('theprimeagen/harpoon')
+ 	use('mbbill/undotree')
+ 	use('tpope/vim-fugitive')
+ 	use('vim-airline/vim-airline')
+ 	use('tpope/vim-commentary')
+ 	use('mg979/vim-visual-multi')
+ 	use('preservim/nerdtree')
+ 	use('ryanoasis/vim-devicons')
+ 	use('Xuyuanp/nerdtree-git-plugin')
 
 
 
